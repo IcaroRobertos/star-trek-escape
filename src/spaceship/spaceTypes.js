b191aa2f56533e6fe354cf653306458bc6a15553
Icaro Roberto <icaro.santos@pravaler.com.br>
2020-05-02 01:47:16 -0300

    Define Types of Spaceships

---
+++ src/spaceship/spaceTypes.js
@@ -0,0 +1,19 @@
+const spaceTypes = [
+  {
+    name: 'Science Vessel - USS Reliant - NCC-1864',
+    description:
+      'Nave estelar classe Miranda, própria para estudos científicos é pequena, com baixo nível de tripulandes, de fácil acesso a ponte de comando',
+  },
+  {
+    name: 'Contitution Class - USS Enterprise - NCC-1701-A',
+    description:
+      'Nave estelar para exploradores da galáxia, com médio nível de tripulandes, de difícil acesso a ponte de comando',
+  },
+  {
+    name: 'Heavy Cruiser - USS Excelsior - NCC-2000',
+    description:
+      'Nave estelar para batalhas galáticas, com alto nível de tripulandes e extrema dificuldade para acessar a ponte de comando',
+  },
+];
+
+module.exports = spaceTypes;
