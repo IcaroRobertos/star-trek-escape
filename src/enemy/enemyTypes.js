86c64fb156d62664d223e22607a6b31e0bfa99ec
Icaro Roberto <icaro.santos@pravaler.com.br>
2020-05-02 02:11:39 -0300

    Define Enemy Types

---
+++ src/enemy/enemyTypes.js
@@ -0,0 +1,18 @@
+const enemyTypes = [
+  {
+    name: 'Klingon',
+    description:
+      "Os klingons são naturais do Planeta Qo'noS. A sociedade klingon é governada pela casta guerreira, que preza a guerra e a honra.",
+    life: 150,
+    damageRange: [5, 9],
+  },
+  {
+    name: 'Romulanos',
+    description:
+      'Os romulanos mantém sorrateiramente uma vigilância sobre os movimentos diplomáticos dos terrestres e sua capacidade de negociação com os demais povos e raças dos quadrantes alfa e beta da galáxia.',
+    life: 100,
+    damageRange: [3, 7],
+  },
+];
+
+module.exports = enemyTypes;
