ca78e8f317382f7b21510547d7da6e66103fb1af
Icaro Roberto <icaro.santos@pravaler.com.br>
2020-05-02 01:29:05 -0300

    Tests for Spaceship Points

--- __tests__/unit/spaceship.test.js
+++ __tests__/unit/spaceship.test.js
@@ -1,5 +1,24 @@
 const spaceship = require('../../src/spaceship/spaceship');
 
 describe('Test Spaceship', () => {
-  it('should test connections between points of spaceship', () => {});
+  it('should test connections between points of spaceship', () => {
+    const shipKeys = Object.keys(spaceship);
+
+    shipKeys.map((key) => {
+      if (spaceship[key].frente) {
+        expect(spaceship[spaceship[key].frente].atras).toEqual(key);
+      }
+      if (spaceship[key].atras) {
+        expect(spaceship[spaceship[key].atras].frente).toEqual(key);
+      }
+      if (spaceship[key].esquerda) {
+        expect(spaceship[spaceship[key].esquerda].direita).toEqual(key);
+      }
+      if (spaceship[key].direita) {
+        expect(spaceship[spaceship[key].direita].esquerda).toEqual(key);
+      }
+
+      return true;
+    });
+  });
 });
